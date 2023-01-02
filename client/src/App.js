e1b3f6a6b1ff245ed3c0dbfba38e92be06124655
Theo Ang <theo.ang816@gmail.com>
2023-01-02 15:20:09 +0700

    add min scale 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 import Cloud from './clouds/Cloud';
-import Ground from './ground/Ground';
 import About from './about/About';
 import Home from './home/Home';
 import Vine from './vines/Vine';
 import Contact from './contacts/Contact';
-import './App.css';
 
 function App() {
   return (
